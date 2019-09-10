aa660fabd3ae61049d21c112479e23ae644ed6e9
kotapiku <ice08235523@gmail.com>
2019-09-10 13:16:24 +0900

    update

--- src/Control/Egison/Core.hs
+++ src/Control/Egison/Core.hs
@@ -16,10 +16,6 @@ module Control.Egison.Core (
   happend,
   (:++:),
   Pattern(..),
-  ValuePat(..),
-  Pair(..),
-  PairPat(..),
-  CollectionPat(..),
   ) where
 
 import           Data.Maybe
@@ -63,7 +59,7 @@ type family as :++: bs :: [*] where
   (a ': as') :++: bs = a ': (as' :++: bs)
 
 --
--- Patterns
+-- Pattern
 --
 
 data Pattern a ctx mt vs where
@@ -72,19 +68,6 @@ data Pattern a ctx mt vs where
   Wildcard :: Pattern a ctx mt '[]
   PatVar :: String -> Pattern a ctx mt '[a]
   AndPat :: Pattern a ctx mt vs -> Pattern a (ctx :++: vs) mt vs' -> Pattern a ctx mt (vs :++: vs')
-  OrPat :: Pattern a ctx mt vs -> Pattern a ctx mt vs -> Pattern a ctx mt vs
+  OrPat  :: Pattern a ctx mt vs -> Pattern a ctx mt vs -> Pattern a ctx mt vs
   NotPat :: Pattern a ctx mt '[] -> Pattern a ctx mt '[]
   PredicatePat :: (HList ctx -> a -> Bool) -> Pattern a ctx mt '[]
-
-class ValuePat mt a where
-  valuePat :: Eq a => (HList ctx -> a) -> Pattern a ctx mt '[]
-
-data Pair a b = Pair a b
-
-class PairPat mt a where
-  pairPat :: a ~ (b1, b2) => mt ~ Matcher (Pair m1 m2) => Pattern b1 ctx (Matcher m1) xs -> Pattern b2 (ctx :++: xs) (Matcher m2) ys -> Pattern a ctx mt (xs :++: ys)
-
-class CollectionPat mt a where
-  nil       :: a ~ [b] => Pattern a ctx mt '[]
-  cons      :: a ~ [b] => mt ~ Matcher (f m) => Pattern b ctx (Matcher m) xs -> Pattern a (ctx :++: xs) mt ys -> Pattern a ctx mt (xs :++: ys)
-  join      :: a ~ [b] => Pattern a ctx mt xs -> Pattern a (ctx :++: xs) mt ys -> Pattern a ctx mt (xs :++: ys)
--- src/Control/Egison/Matcher.hs
+++ src/Control/Egison/Matcher.hs
@@ -3,8 +3,15 @@
 {-# LANGUAGE GADTs                 #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE QuasiQuotes           #-}
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE TypeOperators         #-}
 
 module Control.Egison.Matcher (
+  -- pattern
+  ValuePat(..),
+  Pair(..),
+  PairPat(..),
+  CollectionPat(..),
   -- data type of matcher
   Eql(..),
   Something(..),
@@ -16,25 +23,36 @@ module Control.Egison.Matcher (
   something,
   list,
   multiset,
+  set,
   ) where
 
 import           Control.Egison.Core
 import           Control.Egison.Match
 import           Control.Egison.QQ
+import           Data.List.Unique
+
+--
+-- Pattern
+--
+
+class ValuePat mt a where
+  valuePat :: Eq a => (HList ctx -> a) -> Pattern a ctx mt '[]
+
+data Pair a b = Pair a b
+
+class PairPat mt a where
+  pairPat :: a ~ (b1, b2) => mt ~ Matcher (Pair m1 m2) => Pattern b1 ctx (Matcher m1) xs -> Pattern b2 (ctx :++: xs) (Matcher m2) ys -> Pattern a ctx mt (xs :++: ys)
+
+class CollectionPat mt a where
+  nil :: a ~ [b] => Pattern a ctx mt '[]
+  cons :: a ~ [b] => mt ~ Matcher (f m) => Pattern b ctx (Matcher m) xs -> Pattern a (ctx :++: xs) mt ys -> Pattern a ctx mt (xs :++: ys)
+  join :: a ~ [b] => Pattern a ctx mt xs -> Pattern a (ctx :++: xs) mt ys -> Pattern a ctx mt (xs :++: ys)
 
 --
 -- Matchers
 --
 
-data Eql = Eql
-
 data Something = Something
-
-newtype List a = List a
-newtype Multiset a = Multiset a
-
-eql :: Matcher Eql
-eql = Matcher Eql
 
 integer :: Matcher Eql
 integer = eql
@@ -42,30 +60,29 @@ integer = eql
 something :: Matcher Something
 something = Matcher Something
 
-pair :: Matcher a -> Matcher b -> Matcher (Pair a b)
-pair (Matcher m1) (Matcher m2) = Matcher (Pair m1 m2)
+-- eq matcher
+data Eql = Eql
 
-list :: Matcher a -> Matcher (List a)
-list (Matcher m) = Matcher (List m)
-
-multiset :: Matcher a -> Matcher (Multiset a)
-multiset (Matcher m) = Matcher (Multiset m)
+eql :: Matcher Eql
+eql = Matcher Eql
 
 instance Eq a => ValuePat (Matcher Eql) a where
   valuePat f = Pattern (\tgt ctx _ -> [MNil | f ctx == tgt])
 
-instance Eq a => ValuePat (Matcher (List m)) [a] where
-  valuePat f = Pattern (\tgt ctx _ -> [MNil | f ctx == tgt])
-
-instance (Eq a,  ValuePat (Matcher m) a) => ValuePat (Matcher (Multiset m)) [a] where
-  valuePat f = Pattern (\tgt ctx (Multiset m) ->
-                            match (f ctx, tgt) (pair (list $ Matcher m) (multiset $ Matcher m)) $
-                              [mc| pairPat nil nil => [MNil] |] .*.
-                              [mc| pairPat (cons $x $xs) (cons #x #xs) => [MNil] |] .*.
-                              [mc| pairPat Wildcard Wildcard => [] |] .*. PNil)
+-- pair matcher
+pair :: Matcher a -> Matcher b -> Matcher (Pair a b)
+pair (Matcher m1) (Matcher m2) = Matcher (Pair m1 m2)
 
 instance PairPat (Matcher (Pair m1 m2)) (a1, a2) where
   pairPat p1 p2 = Pattern (\(t1, t2) _ (Pair m1 m2) -> [MCons (MAtom p1 t1 m1) $ MCons (MAtom p2 t2 m2) MNil])
+
+-- list matcher
+list :: Matcher a -> Matcher (List a)
+list (Matcher m) = Matcher (List m)
+
+newtype List a = List a
+instance Eq a => ValuePat (Matcher (List m)) [a] where
+  valuePat f = Pattern (\tgt ctx _ -> [MNil | f ctx == tgt])
 
 instance CollectionPat (Matcher (List m)) [a] where
   nil = Pattern (\t ctx _ -> [MNil | null t])
@@ -75,11 +92,43 @@ instance CollectionPat (Matcher (List m)) [a] where
                                 x:xs -> [MCons (MAtom p1 x m) $ MCons (MAtom p2 xs $ List m) MNil])
   join p1 p2 = Pattern (\tgt ctx m -> map (\(hs, ts) -> MCons (MAtom p1 hs m) $ MCons (MAtom p2 ts m) MNil) (unjoin tgt))
 
+-- multiset matcher
+newtype Multiset a = Multiset a
+
+multiset :: Matcher a -> Matcher (Multiset a)
+multiset (Matcher m) = Matcher (Multiset m)
+
+instance (Eq a,  ValuePat (Matcher m) a) => ValuePat (Matcher (Multiset m)) [a] where
+  valuePat f = Pattern (\tgt ctx (Multiset m) ->
+                            match (f ctx, tgt) (pair (list $ Matcher m) (multiset $ Matcher m)) $
+                              [mc| pairPat nil nil => [MNil] |] .*.
+                              [mc| pairPat (cons $x $xs) (cons #x #xs) => [MNil] |] .*.
+                              [mc| Wildcard => [] |] .*. PNil)
+
 instance CollectionPat (Matcher (Multiset m)) [a] where
   nil = Pattern (\tgt ctx _ -> [MNil | null tgt])
   cons p Wildcard = Pattern (\tgt ctx (Multiset m) -> map (\x -> MCons (MAtom p x m) MNil) tgt)
   cons p1 p2 = Pattern (\tgt ctx (Multiset m) -> map (\(x, xs) -> MCons (MAtom p1 x m) $ MCons (MAtom p2 xs $ Multiset m) MNil) $ matchAll tgt (list $ Matcher m) $ [mc| join $hs (cons $x $ts) => (x, hs ++ ts) |] .*. PNil)
   join p1 p2 = Pattern (\tgt ctx m -> map (\(xs, ys) -> MCons (MAtom p1 xs m) $ MCons (MAtom p2 ys m) MNil) $ concatMap (`unjoinM` tgt) [0..(length tgt)])
+
+-- set matcher
+newtype Set a = Set a
+
+set :: Matcher a -> Matcher (Set a)
+set (Matcher m) = Matcher (Set m)
+
+instance (Eq a,  Ord a, ValuePat (Matcher m) a) => ValuePat (Matcher (Set m)) [a] where
+  valuePat f = Pattern (\tgt ctx (Set m) ->
+                  match (unique (f ctx), unique tgt) (pair (list $ Matcher m) (multiset $ Matcher m)) $
+                    [mc| pairPat nil nil => [MNil] |] .*.
+                    [mc| pairPat (cons $x $xs) (cons #x #xs) => [MNil] |] .*.
+                    [mc| Wildcard => [] |] .*. PNil)
+
+instance CollectionPat (Matcher (Set m)) [a] where
+  nil = Pattern (\tgt ctx _ -> [MNil | null tgt])
+  cons p1 p2 = Pattern (\tgt ctx (Set m) ->
+                  map (\x -> MCons (MAtom p1 x m) $ MCons (MAtom p2 tgt (Set m)) MNil) $ matchAll tgt (list $ Matcher m) $ [mc| join Wildcard (cons $x Wildcard) => x |] .*. PNil)
+  join p1 p2 = undefined
 
 unjoin :: [a] -> [([a], [a])]
 unjoin []     = [([], [])]
