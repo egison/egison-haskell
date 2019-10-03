bae22654e92af2becb0b7628b0946f41d6f43d98
Satoshi Egi <egi@egison.org>
2019-10-03 15:08:27 +0900

    update

--- benchmark/prime-pairs-2.hs
+++ benchmark/prime-pairs-2.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE QuasiQuotes     #-}
 {-# LANGUAGE GADTs           #-}
 
+import System.Environment
 import Control.Egison
 import Data.Numbers.Primes
 
 main :: IO ()
 main = do
-  let n = 100
+  args <- getArgs
+  let n = read (head args)
   let ans = take n (matchAll primes (List Integer)
                      [[mc| join _ (cons $p (cons #(p+2) _)) => (p, p+2) |]])
   putStrLn $ show ans
