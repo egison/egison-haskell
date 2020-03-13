785457c263fc3a99157d6263f40e490d4655f6d2
coord.e <me@coord-e.com>
2020-03-13 18:44:43 +0000

    Compare some benchmarks with egison

--- benchmark/primePairs.hs
+++ benchmark/primePairs.hs
@@ -3,12 +3,34 @@
 import           Control.Egison
 
 import           Data.Numbers.Primes            ( primes )
+import           Language.Egison                ( initialEnv
+                                                , runEgisonExpr
+                                                , EgisonValue
+                                                )
+import           Language.Egison.CmdOptions     ( defaultOption
+                                                , EgisonOpts(..)
+                                                )
 import           Criterion.Main
 
+
+runWithEgison :: String -> IO EgisonValue
+runWithEgison expr = do
+  egisonEnv   <- initialEnv option
+  Right value <- runEgisonExpr option egisonEnv expr
+  pure value
+  where option = defaultOption { optSExpr = False }
 
 primePairs2 :: Int -> [(Int, Int)]
 primePairs2 n = take n
   $ matchAll primes (List Integer) [[mc| _ ++ $p : #(p+2) : _ -> (p, p+2) |]]
+
+primePairs2Egison :: Int -> IO EgisonValue
+primePairs2Egison n = runWithEgison expr
+ where
+  expr =
+    "take "
+      ++ show n
+      ++ " (matchAll primes as list integer with _ ++ $p :: #(p+2) :: _ -> (p, p+2))"
 
 primePairs6 :: Int -> [(Int, Int)]
 primePairs6 n = take n $ matchAll
@@ -16,17 +38,35 @@ primePairs6 n = take n $ matchAll
   (List Integer)
   [[mc| _ ++ $p : _ ++ #(p+6) : _ -> (p, p+6) |]]
 
+primePairs6Egison :: Int -> IO EgisonValue
+primePairs6Egison n = runWithEgison expr
+ where
+  expr =
+    "take "
+      ++ show n
+      ++ " (matchAll primes as list integer with _ ++ $p :: _ ++ #(p+6) :: _ -> (p, p+6))"
+
 main :: IO ()
 main = defaultMain
   [ bgroup
     "(p, p+2) pairs"
-    [ bench "12800" $ nf primePairs2 12800
+    [ bgroup
+      "50"
+      [ bench "egison" $ whnfAppIO primePairs2Egison 50
+      , bench "miniEgison" $ nf primePairs2 50
+      ]
+    , bench "12800" $ nf primePairs2 12800
     , bench "25600" $ nf primePairs2 25600
     , bench "51200" $ nf primePairs2 51200
     ]
   , bgroup
     "(p, p+6) pairs"
-    [ bench "128" $ nf primePairs6 128
+    [ bgroup
+      "50"
+      [ bench "egison" $ whnfAppIO primePairs6Egison 50
+      , bench "miniEgison" $ nf primePairs6 50
+      ]
+    , bench "128" $ nf primePairs6 128
     , bench "256" $ nf primePairs6 256
     , bench "512" $ nf primePairs6 512
     ]
